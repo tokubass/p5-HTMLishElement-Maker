ee2e073905b0ec437b686d64614511f9244bf1e7
tokubass <tokubass@cpan.org>
2014-09-05 01:07:21 +0900

    init

---
+++ lib/HTMLishElement/Maker.pm
@@ -0,0 +1,39 @@
+package HTMLishElement::Maker;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+HTMLishElement::Maker - It's new $module
+
+=head1 SYNOPSIS
+
+    use HTMLishElement::Maker;
+
+=head1 DESCRIPTION
+
+HTMLishElement::Maker is ...
+
+=head1 LICENSE
+
+Copyright (C) tokubass.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokubass E<lt>tokubass@cpan.orgE<gt>
+
+=cut
+
