bf4567f2acfa1580e71d143f9bb13736d35c2c8a
tokubass <tokubass@cpan.org>
2014-09-05 16:24:49 +0900

    update SYNOPSIS

--- lib/HTMLishElement/Maker.pm
+++ lib/HTMLishElement/Maker.pm
@@ -6,7 +6,6 @@ use warnings;
 our $VERSION = "0.01";
 
 
-
 1;
 __END__
 
@@ -14,11 +13,34 @@ __END__
 
 =head1 NAME
 
-HTMLishElement::Maker - It's new $module
+HTMLishElement::Maker - XXX
 
 =head1 SYNOPSIS
 
     use HTMLishElement::Maker;
+    local $HTMLishElement::Maker::ATTR_SETTING = {
+        style => {
+            key_value_sep  => ':', # optional
+            end => ';'  # default ' '
+        }
+    };
+
+    HTMLishElement::Maker->new('<hoge>');# <hoge></hoge>
+    HTMLishElement::Maker->new('<hoge/>');# <hoge/>
+
+    my $anchor = HTMLishElement::Maker->new('<a>');# <a></a>
+    $anchor->push('text'); # <a>text</a>
+    $anchor->add_attr(class => 'hoge'); # <a class="hoge">text</a>
+    $anchor->add_attr(class => [qw/ foo bar /] ); # <a class="hoge foo bar">text</a>
+    $anchor->reset_attr('class'); #<a>text</a>
+
+    $anchor->add_attr(style => { color => 'red', display => 'block'}); # <a style="color:red;display:block;">text</a>
+    $anchor->reset_attr; #remove all attr
+
+    my $div = HTMLishElement::Maker->new('<div>');
+    $div->push($anchor); #<div><a>text</a></div>
+
+    my $str = $div->print;
 
 =head1 DESCRIPTION
 
